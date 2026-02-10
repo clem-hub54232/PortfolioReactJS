7107f91de64d3c5097326367f32f696dd364dd0c
clem <clem.boubou09@gmail.com>
2026-02-10 22:07:06 +0100

    finition contact + home

--- portfolio-reactjs/src/pages/Contact.jsx
+++ portfolio-reactjs/src/pages/Contact.jsx
@@ -1,11 +1,67 @@
 import Page from '../component/Page'
+import { Row, Col, Form, Button } from 'react-bootstrap';
+import '../css/Contact.css'
+
 function Contact() {
     return (
         <>
             <Page
                 title={"Contact"}
                 subtitle={"Pour me contacter en vue d'un entretien ou d'une future collaboration, merci de remplir le formulaire de contact."}>
-                <p>MON BODY</p>
+                <Row className='contact'>
+                    <Col className="contact-form" xs={8}>
+                        <Col className='p-2'>
+                            <h4>Formulaire de contact</h4>
+                            <hr className="form-divider" />
+                            <Form>
+                                <Form.Group className="mb-3 me-3" controlId="exampleForm.ControlInput1">
+                                    <Form.Control className="mb-3" type="text" placeholder="Votre nom" />
+                                    <Form.Control className="mb-3" type="email" placeholder="Votre adresse email" />
+                                    <Form.Control className="mb-3" type="text" placeholder="Votre numéro de téléphone" />
+                                    <Form.Control className="mb-3" type="text" placeholder="Sujet" />
+                                    <Form.Control className="mb-3" as="textarea" placeholder="Votre message" rows={5} />
+                                </Form.Group>
+
+                            </Form>
+                            <div className='formulaire'>
+                                <Button className="mb-3" as="input" type="submit" value="Envoyer" />
+                            </div>
+                        </Col>
+                        <Col className='p-2'>
+                            <h4>Mes coordonnées</h4>
+                            <hr className="form-divider" />
+                            John Doe
+                            <div className="adresse">
+                                <i className="bi bi-map"></i>
+                                <span>40 rue Laure Diebold</span>
+                            </div>
+                            <div className="adresse">
+                                <i className="bi bi-geo-alt"></i>
+                                <span>69009 Lyon, France</span>
+                            </div>
+                            <div className="phone">
+                                <i className="bi bi-phone"></i>
+                                <span>10 20 30 40 50</span>
+                            </div>
+                            <div className="adresse">
+                                <i className="bi bi-envelope-at"></i>
+                                <span>johon.doe@gmail.com</span>
+                            </div>
+                            <div className='carte mt-2'>
+                                <iframe
+                                    title="Google Map"
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5565.254304924361!2d4.7964039775894305!3d45.77866197108078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4eb65edac5b3f%3A0xe01c47049cb2e2b9!2s40%20Rue%20Laure%20Diebold%2C%2069009%20Lyon%2C%20France!5e0!3m2!1sfr!2sca!4v1770757327814!5m2!1sfr!2sca"
+                                    width="100%"
+                                    height="100%"
+                                    style={{ border: 0 }}
+                                    allowFullScreen=""
+                                    loading="lazy"
+                                    referrerPolicy="no-referrer-when-downgrade"
+                                />
+                            </div>
+                        </Col>
+                    </Col>
+                </Row>
             </Page></>
     )
 }
--- portfolio-reactjs/src/pages/Home.jsx
+++ portfolio-reactjs/src/pages/Home.jsx
@@ -1,6 +1,113 @@
+import { useEffect, useState } from 'react';
+
+import { Row, Col, Modal, Button, ProgressBar } from 'react-bootstrap';
+
+import '../css/Home.css'
+import image from '../img/john-doe-about.jpg'
+
 function Home() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [items, setItems] = useState([]);
+  const getItems = async () => {
+    const res = await fetch("https://api.github.com/users/github-john-doe");
+    const json = await res.json();
+    setItems(json);
+  }
+
+  useEffect(() => {
+    getItems();
+  }, [])
+  console.log(items)
+
   return (
-    <></>
+    <>
+      <section className="hero">
+        <div className="hero-content">
+          <h1>Bonjour je suis John Doe</h1>
+          <h2>Développeur web full stack</h2>
+          <Button variant="danger" onClick={handleShow}>
+            En savoir plus
+          </Button>
+
+          <Modal show={show} onHide={handleClose} centered data-bs-theme="dark">
+            <Modal.Header closeButton>
+              <Modal.Title>Mon profil GitHub</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Row className='profil-github'>
+                <Col md={6}>
+                  <img src={items.avatar_url} width={200} height={200} />
+                </Col>
+                <Col md={6}>
+                  <div className="profil">
+                    <i className="bi bi-person"></i>
+                    <span>  <a href={items.html_url} >{items.name} </a></span>
+                  </div>
+                  <div className="profil">
+                    <i className="bi bi-geo-alt"></i>
+                    <span></span>
+                  </div>
+                  <div className="profil">
+                    <i className="bi bi-card-text"></i>
+                    <span>{items.bio}</span>
+                  </div>
+                  <div className="profil">
+                    <i className="bi bi-box"></i>
+                    <span>Repositories : {items.public_repos}</span>
+                  </div>
+                  <div className="profil">
+                    <i className="bi bi-people"></i>
+                    <span>Followers : {items.followers}</span>
+                  </div>
+                  <div className='following'>
+                    <i className="bi bi-people"></i>
+                    <span>Following : {items.following}</span>
+                  </div>
+                </Col>
+              </Row>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Fermé
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
+      </section>
+      <Row className='home'>
+        <Col className="presentation mt-5 mb-5" xs={8}>
+          <Col className='p-2'>
+            <h4>A propos</h4>
+            <hr className="home-divider" />
+            <img src={image} width="auto" height={200} />
+            <p>Lorem ipsum dolor sit amet consecteur adipisicing elit. Optio, necessitatibus consectetur tempore perferendis nostrum, ex delectus reiciendis impedit aut iure enim placeat? Natus, neque at?</p>
+            <p>Lorem ipsum dolor sit amet consecteur adipisicing elit. Optio, necessitatibus consectetur tempore perferendis nostrum, ex delectus reiciendis impedit aut iure enim placeat? Natus, neque at?</p>
+            <p>Lorem ipsum dolor sit amet consecteur adipisicing elit. Optio, necessitatibus consectetur tempore perferendis nostrum, ex delectus reiciendis impedit aut iure enim placeat? Natus, neque at?</p>
+
+
+          </Col>
+          <Col className='p-2'>
+            <h4>Mes compétences</h4>
+            <hr className="home-divider" />
+            <span>HTML5 90%</span>
+            <ProgressBar className='mb-3' variant="danger" now={90} />
+            <span>CSS3 80%</span>
+            <ProgressBar className='mb-3' variant="info" now={80} />
+            <span>JAVASCRIPT 70%</span>
+            <ProgressBar className='mb-3' variant="warning" now={70} />
+            <span>PHP 60%</span>
+            <ProgressBar className='mb-3' variant="success" now={60} />
+            <span>REACT 50%</span>
+            <ProgressBar className='mb-3' now={50} />
+          </Col>
+        </Col>
+      </Row>
+    </>
+
   )
 }
 
